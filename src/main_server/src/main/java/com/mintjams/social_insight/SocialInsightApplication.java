7805b130925af692ae930008858d10d39842fa6b
turbine244 <144324883+turbine244@users.noreply.github.com>
2024-11-19 19:05:14 +0900

    비동기 오류 수정

--- src/main_server/src/main/java/com/mintjams/social_insight/SocialInsightApplication.java
+++ src/main_server/src/main/java/com/mintjams/social_insight/SocialInsightApplication.java
@@ -2,6 +2,7 @@ package com.mintjams.social_insight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling

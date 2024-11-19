00bcab5073c109aec0f44bae4dda6e44988eb647
turbine244 <144324883+turbine244@users.noreply.github.com>
2024-11-19 17:41:45 +0900

    비동기화 관련

--- src/main_server/src/main/java/com/mintjams/social_insight/SocialInsightApplication.java
+++ src/main_server/src/main/java/com/mintjams/social_insight/SocialInsightApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableScheduling
 public class 	SocialInsightApplication {
 
 	public static void main(String[] args) {
